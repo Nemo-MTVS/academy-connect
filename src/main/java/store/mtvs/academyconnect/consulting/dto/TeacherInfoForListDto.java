cd9b304372287e65b4ca8b5685313423f544adb1
choe-yujin <dev.yujinchoe@gmail.com>
2025-04-29 05:39:38 +0900

    feat: 학생 예약 조회 기능 구현 위한 DTO 추가

---
+++ src/main/java/store/mtvs/academyconnect/consulting/dto/TeacherInfoForListDto.java
@@ -0,0 +1,17 @@
+package store.mtvs.academyconnect.consulting.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TeacherInfoForListDto {
+    private String id;
+    private String name;
+    private String classGroupName;
+    private String profileImageUrl;
+}
