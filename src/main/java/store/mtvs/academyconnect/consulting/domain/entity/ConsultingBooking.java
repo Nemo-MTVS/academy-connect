e0dfd06af42c04f14cbee88966f8b9138d472930
choe-yujin <dev.yujinchoe@gmail.com>
2025-04-30 19:42:41 +0900

    refactor: 예약 취소시 강사 슬롯 상태 변경하기 위해 setter 추가

--- src/main/java/store/mtvs/academyconnect/consulting/domain/entity/ConsultingBooking.java
+++ src/main/java/store/mtvs/academyconnect/consulting/domain/entity/ConsultingBooking.java
@@ -1,9 +1,6 @@
 package store.mtvs.academyconnect.consulting.domain.entity;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import store.mtvs.academyconnect.user.domain.entity.User;
 
 import jakarta.persistence.*;
@@ -12,6 +9,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "consulting_bookings")
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ConsultingBooking {
 
